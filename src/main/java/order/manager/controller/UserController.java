31802f9e5da03f3fc49c5e551cc799a708d50d2c
陈涛 <chentao01@corp.netease.com>
2018-02-23 18:47:29 +0800

    重定向登入界面

--- src/main/java/order/manager/controller/UserController.java
+++ src/main/java/order/manager/controller/UserController.java
@@ -10,18 +10,12 @@ public class UserController {
 
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
-        if (error != null)
-            model.addAttribute("error", "Your username and password is invalid.");
-
-        if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
-
-        return "login.jsp";
+    public String login() {
+        return "login.html";
     }
 
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
     public String welcome(Model model) {
-        return "welcome.jsp";
+        return "index.html";
     }
 }
