4902cf5c3738ec8c30c2a14a1dbf11ad6235366c
陈涛 <chentao01@corp.netease.com>
2018-02-24 16:35:04 +0800

    session过期时间设置

--- src/main/webapp/resources/src/components/HelloWorld.vue
+++ src/main/webapp/resources/src/components/HelloWorld.vue
@@ -1,87 +1,20 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
+
+    <div class="hd_operate">
+      欢迎，<el-dropdown trigger="click">
+      <!--<a href="javascript:;" class="el-dropdown-link"><@security.authentication property="details.realName"/></a>-->
+      <el-dropdown-menu slot="dropdown">
+        <a href="/logout">
+          <el-dropdown-item>退出</el-dropdown-item>
         </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-    你大爷
+      </el-dropdown-menu>
+    </el-dropdown>
+    </div>
   </div>
+
+
 </template>
 
 <script>
