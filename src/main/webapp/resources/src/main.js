b98ab4fadf9f3ef39c98d69bc3a855fbeeb2987f
张祥闰 <hzzhangxiangrun@corp.netease.com>
2018-03-13 17:44:51 +0800

    增加babel-polyfill

--- src/main/webapp/resources/src/main.js
+++ src/main/webapp/resources/src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/base.css'
+import 'babel-polyfill'
 
 
 Vue.use(Element, { size: 'medium' })
